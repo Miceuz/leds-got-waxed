290791465119d93592ca78e05a71d71b8707a726
miceuz <mic@wemakethings.net>
2016-01-02 14:24:21 +0200

    Make test mode really work

--- firmware/src/DipConfig.h
+++ firmware/src/DipConfig.h
@@ -1,6 +1,7 @@
 #ifndef _DIP_CONFIG_H
 #define _DIP_CONFIG_H
 #include <inttypes.h>
+#include <Arduino.h>
 
 class DipConfig
 {
@@ -8,8 +9,7 @@ public:
 	DipConfig(uint8_t _latchPin, uint8_t _clkPin, uint8_t _dataPin);
 	uint16_t get();
 	inline uint8_t isTestMode() {
-//		return 0 != (get() & 1);
-		return get() & (1 << 11);
+		return 0 != (get() & (uint16_t)(1 << 11));
 	};
 private:
 	uint8_t latchPin;	
